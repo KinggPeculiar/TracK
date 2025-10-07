6c6e511f9b67d3f7e71430a2596e74aafb5a748c
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-10-07 03:04:31 +0100

    style(RootLayout.jsx):  set div and main elements to width: full-screen, take out padding and margin

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,9 +5,9 @@ import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-[#031335] w-full">
       <Navbar />
-      <main className="flex-1 container mx-auto px-4 py-6">
+      <main className="flex-1 container mx-auto max-w-screen">
         <Outlet />
       </main>
       <Footer />
