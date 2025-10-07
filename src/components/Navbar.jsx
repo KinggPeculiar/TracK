e034b9918bd5506ba0bfe87751e7569e1c75b5d1
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-10-07 03:01:17 +0100

    feat(Navbar.jsx): add and modify brand logo to home link, delete previous home link. style(Navbar.jsx): add background-color, modify logo style.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const linkClasses = "relative text-white no-underline hover:text-blue-400 transition-colors duration-300 before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-blue-400 before:transition-all before:duration-300 hover:before:w-full";
+
 function Navbar() {
   return (
-    <nav style={{
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: "1rem",
-      backgroundColor: "#0077cc",
-      color: "white"
-    }}>
-      <h2>POS Tracker</h2>
+    <nav className="bg-[#030b1d] flex justify-between items-center p-4">
+      <Link to="/" className="text-[#ff0004] text-4xl font-bold hover:cursor-pointer ">Trac<span className="text-[#25b764]">K</span></Link>
       <div style={{ display: "flex", gap: "1rem" }}>
-        <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
-        <Link to="/signin" style={{ color: "white", textDecoration: "none" }}>Sign In</Link>
-        <Link to="/signup" style={{ color: "white", textDecoration: "none" }}>Sign Up</Link>
-        <Link to="/dashboard" style={{ color: "white", textDecoration: "none" }}>Dashboard</Link>
+        <Link to="/signin" className={linkClasses}>Sign In</Link>
+        <Link to="/signup" className={linkClasses}>Sign Up</Link>
+        <Link to="/dashboard" className={linkClasses}>Dashboard</Link>
       </div>
     </nav>
   );
