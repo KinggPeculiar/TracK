6e81d04dac90ecb7df295832925c4cd4e93e0ea1
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-10-15 22:49:34 +0100

    refractor(logo): extract logo into reusable component.

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Logo() {
+    return (
+        <div className="w-full">
+            <h1 className="text-4xl font-bold mb-2 md:text-left">
+                <span className="text-[#FF0004]">Trac</span>
+                <span className="text-[#25B764]">K</span>
+            </h1>             
+        </div>        
+    );
+}
